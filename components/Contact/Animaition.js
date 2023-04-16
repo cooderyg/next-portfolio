238f10ff8120168439a5e1cb33e6c8c057abbb66
cooderyg <ygk9604@naver.com>
2023-04-16 18:51:05 +0900

    404페이지 완성

--- components/Contact/Animaition.js
+++ components/Contact/Animaition.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Lottie from 'react-lottie-player'
-import lottieJson from '@/public/animaition2.json'
+import lottieJson from '@/public/animation2.json'
 
 export default function Animation() {
   return (
--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,13 @@
+import Animation from "@/components/404/Animation";
+import Layout from "@/components/Home/Layout";
+
 export default function PageNotFound() {
     return(
-        <>
-            <h1>페이지를 찾을 수 없습니다. ㅜㅜ</h1>
-        </>
+        <Layout>
+            <h2 className="text-center mt-5 font-bold text-xl md:text-3xl lg:text-4xl px-2">페이지를 찾을 수 없습니다 ..ㅜㅜ</h2>
+            <div className="min-h-[400px] lg:min-h-screen">
+                <Animation />
+            </div>
+        </Layout>
     )
 }
