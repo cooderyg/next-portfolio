2d29971f398ed49de8f5674516e9dd9f86e9a46e
cooderyg <ygk9604@naver.com>
2023-04-18 14:18:45 +0900

    about-me mark up

--- pages/about-me.js
+++ pages/about-me.js
@@ -3,9 +3,28 @@ import Layout from "@/components/Home/Layout";
 export default function AboutMe() {
     return(
         <Layout>
-            <h2>주요기술</h2>
-            <h2>서브기술</h2>
-            <h2>공부중인 기술</h2>
+            <section>
+                <h2>저는 이런 인재입니다!</h2>
+                <ul>
+                    <li>새로운 것에 거리낌이 없고, 스스로 찾아 자기주도학습을 할 수 있습니다.</li>
+                    <li>문제가 발생했을 때 해결하는 과정에서 성장하는 것을 즐깁니다.</li>
+                    <li>타인의 이야기를 경청하며 피드백을 적극 수용합니다.</li>
+                </ul>
+            </section>
+            <section>
+                <h2>주요기술(notion api로 바꾸기)</h2>
+                <ul>
+                    <li>React</li>
+                    <li>Next.js</li>
+                    <li>웹표준</li>
+                    <li>JS</li>
+                    <li>JQery</li>
+                    <li>Tailwind CSS</li>
+                    <li>SCSS</li>
+                    <li>SEO</li>
+                    <li>Figma</li>
+                </ul>
+            </section>
         </Layout>
     );
 }
